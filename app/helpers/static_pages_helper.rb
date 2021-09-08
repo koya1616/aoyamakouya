cf7e8c81625d51e39b2fd347f41d920d888a7829
koya1616 <k_haru_love@i.softbank.jp>
2021-09-08 11:33:11 +0000

    静的ページ作成

---
+++ app/helpers/static_pages_helper.rb
@@ -0,0 +1,19 @@
+module StaticPagesHelper
+    def home
+        
+    end
+    
+    def help
+        
+    end
+
+    def about
+        
+    end
+
+    def contact
+
+    end    
+    
+    
+end
